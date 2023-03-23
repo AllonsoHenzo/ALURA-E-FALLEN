632c66b82fd2f137425675c6dd7d0a76d60f2b7b
Allonso Henzo <allonso.henzo@gmail.com>
2023-03-22 21:45:10 -0300

    adicionando java - Aula dia 2

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log ("hello world")
+const team1= "Lonf";
+const team2= "Trybe";
+let turn = team1;
+const turnText = document.querySelector("#turnText");
+turnText.innerText="É a vez da equipe "+turn+" banir o mapa";
+const maps= document.querySelectorAll(".card");
+function choosemap(event) {
+    console.log(event.currentTarget);
+    if(turn==team1){
+        turn=team2
+    } else{
+        turn=team1
+    } 
+    turnText.innerText="É a vez da equipe "+turn+" banir o mapa";
+}
+for (let index=0;index < maps.length; index++){
+    maps[index].addEventListener("click",choosemap)
+}
