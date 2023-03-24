5d5b101c5c808278ad9cff48ee4002c2ef78c6ff
Allonso Henzo <allonso.henzo@gmail.com>
2023-03-23 22:53:39 -0300

    finalizando projeto - Dia 3 Aula  final

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const team1= "Lonf";
 const team2= "Trybe";
 let turn = team1;
+let mapPool = ["Train", "Mirage", "Cache", "Overpass", "Cobblestone"]
 const turnText = document.querySelector("#turnText");
 turnText.innerText="É a vez da equipe "+turn+" banir o mapa";
 const maps= document.querySelectorAll(".card");
 function choosemap(event) {
-    console.log(event.currentTarget);
     if(turn==team1){
         turn=team2
     } else{
         turn=team1
     } 
+
     turnText.innerText="É a vez da equipe "+turn+" banir o mapa";
+    event.currentTarget.classList.add("selected")
+    event.currentTarget.removeEventListener("click", choosemap);
+    event.currentTarget.querySelector(".accept").innerText="VETADO";
+    const clickedMap = event.currentTarget.querySelector(".map-name").innerText;
+    mapPool = mapPool.filter(map => map != clickedMap)
+    
+    if (mapPool.length==1) {
+        const decidedMap = document.querySelector(".card:not(.selected)");
+        decidedMap.classList.add("picked");
+        decidedMap.removeEventListener("click", choosemap);
+        decidedMap.classList.add("disable-hover");
+        turnText.innerText=`O mapa da partida será ${mapPool[0]}`
+
+    }
 }
-for (let index=0;index < maps.length; index++){
+
+for (let index=0; index < maps.length; index++){
     maps[index].addEventListener("click",choosemap)
 }
